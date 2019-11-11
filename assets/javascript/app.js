e0eea90a4b0b4f6d0ef370176400780528b74875
PelicanSkeleton <sgphillips80@gmail.com>
2019-11-10 23:25:56 -0500

    This is still not functioning but it's better than what I originally turned in.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,23 +1,58 @@
 var timeLeft = 10;
 var interval;
-var questionDiv = "<div class= 'question'> <h1>What year is the first season of Deadwood set in?</h1><input type='radio' name='q1' value='a'>a. 1872<br><input type='radio' name='q1' value='b'>b. 1876<br><input type='radio' name='q1' value='c'>c. 1901<br><input type='radio' name='q1' value='d'>d. 1856<br></div>";
-var questions = ["What year is the first season of Deadwood set in?", "How many f-bombs were used throughout the series?", "What was Calamity Jane's real name?", "How many episodes were there in the series?"];
 
+var question = [{
+    question: "What year is the first season of Deadwood set in?",
+    answers: ["a. 1872", "b. 1876", "c. 1901", "d. 1856"],
+    answer: "b"
+},
+{
+    question: "How many f-bombs were used throughout the series?",
+    answers: ["", "", "", ""],
+    answer: ""
+},
+{
+    question: "What was Calamity Jane's real name?",
+    answers: ["", "", "", ""],
+    answer: ""
+},
+{
+    question: "How many episodes were there in the series?",
+    answers: ["", "", "", ""],
+    answer: ""
+}]
 
 $("#start-button").on("click", function () {
     hideStart();
     run();
-    
-    $(".game").append(questionDiv);
-    
-    
+
+    //build the div
+    //loop through answer array for radio buttons
+    for (i = 0; i < question.length; i++) {
+
+        var div = $("<div>");
+        div.addClass("question");
+        div.html("<h1>" + question[i].question + "</h1>")
+        var radio = $("<input>");
+        radio.attr("type", "radio");
+        radio.attr("name", "q1");
+        radio.attr("value", question[i].answers[0, 1, 2, 3]);
+        radio.text(question[i].answers[0, 1, 2, 3]);
+        div.append(radio);
+        $(".game").append(div);
+
+    };
+
+    // $(".game").append(questionDiv);
 
 
 
 
 
 
-    console.log(questionDiv);
+
+
+    console.log(div);
     console.log(interval);
     console.log(timeLeft);
 });
@@ -29,32 +64,34 @@ function hideStart() {
 function run() {
     clearInterval(interval);
     interval = setInterval(decrement, 1000);
-    
+
 }
 
 function decrement() {
     timeLeft--;
     console.log(timeLeft + " decrement")
     $("#show-time").html("<h1>" + timeLeft + "</h1>");
-    
-    
-    if(timeLeft === 0) {
+
+
+    if (timeLeft === 0) {
         stop();
         $(".game").html("<h1>TIMES UP!</h1>");
-        next();
+
 
     }
-    
+
 }
 
-function next() {
-    $(".game").append("<button id='next'>Next</button>");
-    for(i = 1; i <= questions[i]; i ++) {
-     $("#next").on("click", function(){
-        $(".game").html("<h1>" + question[i] + "</h1>");
-     });   
-    }
-}
+// function next() {
+//     $(".game").append("<button id='next'>Next</button>");
+//     for (i = 1; i <= question.length; i++) {
+//         $("#next").on("click", function () {
+//             $(".game").html("<h1>" + question[i] + "</h1>");
+//             console.log(question[i]);
+//             console.log(indexof(question[i]));
+//         });
+//     }
+// }
 
 function stop() {
     clearInterval(interval);
